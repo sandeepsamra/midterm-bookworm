35095e7e5280914ce6b29cbef7451394780c3c8a
nfamula <nfamula@gmail.com>
2016-01-27 20:29:47 +0000

    added books/index, books/new

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,33 @@
 get '/' do
   erb :index
 end
+
+get '/books' do
+  @books = Book.all
+  erb :'books/index'
+end
+
+#Add a book
+get '/books/new' do
+  @book = Book.new
+  erb :'books/new'
+end
+
+post '/books' do
+  @book = Book.new(
+    author:   params[:author],
+    title: params[:title],
+    genre:  params[:genre]
+  )
+  if @book.save
+    redirect '/books'
+  else
+    erb :'books/new'
+  end 
+end
+
+#logout
+get '/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
