944cb5304ef0be9c5166671ce181a9b3eca74388
nfamula <nfamula@gmail.com>
2016-01-29 18:02:56 +0000

    button

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
 belongs_to :user
 
-
-
+validates :title, presence: true
+validates :author, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :books
 
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /.*@.*\..*/, :on => :create
+  validates :password, presence: true
 end
